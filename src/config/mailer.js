38ae14b8d39e0ad9d312f0525a740e738965a895
Jhordany2004 <168252222+Jhordany2004@users.noreply.github.com>
2026-01-29 20:50:36 -0500

    update mailer v2

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const dns = require('node:dns');
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -11,6 +14,7 @@ const transporter = nodemailer.createTransport({
     tls: {
         rejectUnauthorized: false
     },
+    family: 4,
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
